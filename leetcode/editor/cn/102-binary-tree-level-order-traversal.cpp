2282f04813811e5e5ba446962e2b62aacc3630ac
jaychang <jaychang1987@gmail.com>
2023-06-21 14:51:32 +0800

    feat: [104]二叉树最大深度

---
+++ leetcode/editor/cn/102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,99 @@
+//给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[9,20],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 广度优先搜索 二叉树 
+// 👍 1711 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        vector<vector<int>> res;
+        if (!root) {
+            return res;
+        }
+        queue<TreeNode *> q;
+        TreeNode *node;
+        for (q.push(root); !q.empty();) {
+            res.push_back(vector<int>());
+            int currentLevelSize = q.size();
+            for (int count = 0; !q.empty() && count < currentLevelSize; count++) {
+                node = q.front();
+                q.pop();
+                res.back().push_back(node->val);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return res;
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    auto list = {1, 2, 3, 4, 5, 6, 7};
+    Tree *tree = new Tree(list);
+    vector<vector<int>> res = s.levelOrder(tree->root);
+    for (auto i = res.begin(); i < res.end(); i++) {
+        vector<int> v = *i;
+        cout << "[";
+        for (auto j = v.begin(); j < v.end(); j++) {
+            cout << *j << " ";
+        }
+        cout << "]" << endl;
+    }
+
+}
---
+++ leetcode/editor/cn/104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,81 @@
+//给定一个二叉树，找出其最大深度。 
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 1630 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // 方法一： 递归法（DFS深度优先）
+//    int maxDepth(TreeNode *root) {
+//        if (!root) {
+//            return 0;
+//        }
+//        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+//    }
+
+    // 方法二： BFS广度优先
+    int maxDepth(TreeNode *root) {
+        if (!root) {
+            return 0;
+        }
+
+        queue<TreeNode *> q;
+        TreeNode *node;
+        int maxDepth = 0, currentLevelSize;
+        for (q.push(root); !q.empty(); maxDepth++) {
+            currentLevelSize = q.size();
+            for (int count = 0; count < currentLevelSize; count++) {
+                node = q.front();
+                q.pop();
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return maxDepth;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    auto list = {1, 2, 3, 4, 5, 6, 7, 8};
+    Tree *tree = new Tree(list);
+    int maxDepth = s.maxDepth(tree->root);
+    cout << maxDepth << endl;
+}
