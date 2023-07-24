0394c3b097a3cfa4c7611103cb8466429a2d0ed7
jaychang <jaychang1987@gmail.com>
2023-07-24 10:11:08 +0800

    feat: 将有序数组转为二叉搜索树

---
+++ leetcode/editor/cn/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,81 @@
+//给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。 
+//
+// 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-10,-3,0,5,9]
+//输出：[0,-3,9,-10,null,5]
+//解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+//
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,3]
+//输出：[3,1]
+//解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 104 
+// -104 <= nums[i] <= 104 
+// nums 按 严格递增 顺序排列 
+// 
+// Related Topics 树 二叉搜索树 数组 分治 二叉树 
+// 👍 1346 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums) {
+        if (nums.size() == 0) {
+            return nullptr;
+        }
+        return traverse(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode *traverse(vector<int> &nums, int startIndex, int endIndex) {
+        if (startIndex > endIndex) {
+            return nullptr;
+        }
+        int mid = (startIndex + endIndex) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = traverse(nums, startIndex, mid - 1);
+        root->right = traverse(nums, mid + 1, endIndex);
+        return root;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    vector<int> l = {-10, -3, 0, 5, 9};
+    TreeNode *root = s.sortedArrayToBST(l);
+    print_tree(root);
+}
---
+++ leetcode/editor/cn/124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,81 @@
+//二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定
+//经过根节点。 
+//
+// 路径和 是路径中各节点值的总和。 
+//
+// 给你一个二叉树的根节点 root ，返回其 最大路径和 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3]
+//输出：6
+//解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [-10,9,20,null,null,15,7]
+//输出：42
+//解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围是 [1, 3 * 104] 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 动态规划 二叉树 
+// 👍 2009 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        int res = INT32_MIN;
+        traverse(root, res);
+        return res;
+    }
+
+    int traverse(TreeNode *root, int &res) {
+        if (!root) {
+            return 0;
+        }
+        int leftPathSum = traverse(root->left, res);
+        int rightPathSum = traverse(root->right, res);
+        int maxPathSum = max({leftPathSum + root->val, rightPathSum + root->val, root->val});
+        res = max({res, maxPathSum, leftPathSum + rightPathSum + root->val});
+        return maxPathSum;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    auto l = {-10, 9, 20, -1, -1, 15, 7};
+    Tree *tree = new Tree(l);
+    int res = s.maxPathSum(tree->root);
+    cout << res << endl;
+}
